748181288843820fce741d8e630b98ee20c86820
Mehrdad Shirvan <mysmartapply.it@gmail.com>
2023-07-04 18:55:10 +0330

    manage route

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-urlspattern = [
+urlpatterns = [
     path('',views.index, name="blog-index"),
     path("posts",views.posts, name="blog-posts"),
     path("posts/<slug:slug>",views.single_post, name="blog-single-post")
