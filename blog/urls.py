f05a962e5e8e5e5200600ee4c39a8f214ee0ae35
Mehrdad Shirvan <mysmartapply.it@gmail.com>
2023-07-04 19:17:08 +0330

    template manage

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name="blog-index"),
+    path("",views.index, name="blog-index"),
     path("posts",views.posts, name="blog-posts"),
     path("posts/<slug:slug>",views.single_post, name="blog-single-post")
 ]
