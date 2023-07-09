aa9d067db66c72d87fd328a98b59b6547174ac9f
Mehrdad Shirvan <mysmartapply.it@gmail.com>
2023-07-09 21:17:56 +0330

    django admin add country & address modal

--- blog/admin.py
+++ blog/admin.py
@@ -1,15 +1,22 @@
 from django.contrib import admin
-from .models import Product, Customer
+from .models import Product, Customer, Address, Country
 
 
+# Register your models here.
 # Register your models here.
 
 class ProductAdmin(admin.ModelAdmin):
     readonly_fields = ("slug",)
-    list_filter = ("name",)
+    list_filter = ("name","customer")
     list_display = ('id',"name")
     # prepopulated_fields = {"slug": ("name",)}
 
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('id',"name")
+
+
 admin.site.register(Product, ProductAdmin)
-admin.site.register(Customer)
+admin.site.register(Customer,CustomerAdmin)
+admin.site.register(Address)
+admin.site.register(Country)
 
