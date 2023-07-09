45a4d0e3cc998bbcdc696e61e2e7c601696250a5
Mehrdad Shirvan <mysmartapply.it@gmail.com>
2023-07-09 19:33:55 +0330

    change url set slug

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("",views.index, name="blog-index"),
+    path("create",views.create, name="blog-create"),
     path("posts",views.posts, name="blog-posts"),
     path("posts/<slug:slug>",views.single_post, name="blog-single-post")
 ]
