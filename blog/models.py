5f21186c556c4447d9d57ed022ff740dfc08c028
Mehrdad Shirvan <mysmartapply.it@gmail.com>
2023-07-09 20:53:28 +0330

    make relation between table in database in model + migration file

--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,18 @@ from django.urls import reverse
 from django.utils.text import slugify
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+
+class Customer(models.Model):
+    id = models.BigAutoField
+    name = models.CharField(max_length=100)
+
+
 # Create your models here.
 class Product(models.Model):
     id = models.BigAutoField
     name = models.CharField(max_length=150)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True)
     slug = models.SlugField(default="",null=False, blank=True,editable=False, db_index=True)
-
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super().save(*args, **kwargs)
