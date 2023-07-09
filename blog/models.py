e8be2d64b7b8608d650b97a6670661d8986da6c4
Mehrdad Shirvan <mysmartapply.it@gmail.com>
2023-07-09 21:17:34 +0330

    add country modal add address modal relation 1*1 & many*many

--- blog/models.py
+++ blog/models.py
@@ -3,18 +3,41 @@ from django.urls import reverse
 from django.utils.text import slugify
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+class Country(models.Model):
+    id = models.BigAutoField
+    name = models.CharField(max_length=100)
+    class Meta:
+        verbose_name_plural = 'Countries'
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Address(models.Model):
+    id = models.BigAutoField
+    full_address = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.full_address}"
+
+    class Meta:
+        verbose_name_plural = 'Address Entries'
 
 class Customer(models.Model):
     id = models.BigAutoField
     name = models.CharField(max_length=100)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
 
+    def __str__(self):
+        return f"{self.name}"
 
 # Create your models here.
 class Product(models.Model):
     id = models.BigAutoField
     name = models.CharField(max_length=150)
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True, related_name="customer")
     slug = models.SlugField(default="",null=False, blank=True,editable=False, db_index=True)
+    published_countries = models.ManyToManyField(Country,)
+
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super().save(*args, **kwargs)
@@ -24,3 +47,8 @@ class Product(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.id})"
+
+
+
+
+
