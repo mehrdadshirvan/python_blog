ca0376d635aea0a610b97775fcfc6294cef44857
Mehrdad Shirvan <mysmartapply.it@gmail.com>
2023-07-09 19:34:17 +0330

    add list post and single post template

--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,39 @@
 import mysql.connector.connection
 from MySQLdb import OperationalError
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 import requests
 import json
 from django.db import connections
 from django.http import HttpResponse
 
 # Create your views
-from blog.models import products
-from my_site import settings
+from blog.models import Product
 
 
 def index(request):
     return render(request, "blog/index.html")
 
 def posts(request):
-    db_conn = connections['default']
-    try:
-        c = db_conn.cursor()
-    except OperationalError:
-        connected = False
-    else:
-        connected = True
+    posts = Product.objects.all()
+    return render(request, "blog/posts.html",{'posts':posts})
 
-    pass
-    # return render(request, "blog/posts.html", {'products': pro})
+def create(request):
+    new = Product(name='testw').save()
+    # posts = Product.objects.all()
+    # new = Product.objects.all()[0]
+    # new.name = 'mehrdad'
+    # new.save()
+    # new.delete()
+    # p = Product.objects.get(id='2')
+    # p.name = 'mehrdad2'
+    # p.save()
 
-def single_post(request):
-    pass
+    return render(request, "blog/posts.html",)
+
+def single_post(request,slug):
+    post = get_object_or_404(Product, slug=slug)
+    Product.objects.get(slug=slug).save()
+    return render(request, "blog/posts-single.html",{
+        'name':post.name,
+    })
+
