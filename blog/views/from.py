57bef09e1340b67ad8485e70c4ba819b7635ea16
Mehrdad Shirvan <mysmartapply.it@gmail.com>
2023-07-10 18:30:29 +0330

    rem f

--- blog/views/from.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector.connection
-from MySQLdb import OperationalError
-from django.shortcuts import render, redirect, get_object_or_404
-import requests
-import json
-from django.db import connections
-from django.http import HttpResponse
-
-
