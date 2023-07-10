a7917f9a1d658cc9d578eef6d7931bf69ee43c22
Mehrdad Shirvan <mysmartapply.it@gmail.com>
2023-07-10 18:30:42 +0330

    validation form

---
+++ blog/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Customer
+
+class CustomerForm(forms.Form):
+    name = forms.CharField(label="Customer name", max_length=100, error_messages={
+        "required" : "this field is required!",
+        "max_length": "name is too long"
+    })
+
+    class Meta:
+        model = Customer
+        fields = '__all__'
